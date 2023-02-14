4dfdab0f96c42067e393fd661f18b1eb1f5a58e3
tae2On <djs99870@naver.com>
2023-02-14 15:34:53 +0900

    Update 4.py

--- 4.py
+++ 4.py
@@ -57,68 +57,69 @@ error_prev = 0.
 time_prev = 0.
 
 try:
-    # 원하는 모터 각도 (반복 입력 가능하게 수정해야 함.)
-    setha = int(input('각도를 입력하시오 : '))
+    while True: 
+        # 원하는 모터 각도 (반복 입력 가능하게 수정해야 함.)
+        setha = int(input('각도를 입력하시오 : '))
 
-    while True:
-        # motorDeg : 실제 모터 각도
-        motorDeg = encoderPos * ratio
-        # error : 원하는 각도 - 실제 모터 각도
-        error = setha - motorDeg
-        de = error - error_prev
-        dt = time.time() - time_prev
+        while True:
+            # motorDeg : 실제 모터 각도
+            motorDeg = encoderPos * ratio
+            # error : 원하는 각도 - 실제 모터 각도
+            error = setha - motorDeg
+            de = error - error_prev
+            dt = time.time() - time_prev
     
-        control = (kp * error) + (kd * de/dt) + (ki * error * dt)
+            control = (kp * error) + (kd * de/dt) + (ki * error * dt)
 
-        error_prev = error
-        time_prev = time.time()
+            error_prev = error
+            time_prev = time.time()
 
-        # 역방향
-        if(setha < 0) :
-            IO.output(AIN1, IO.LOW)
-            IO.output(AIN2, IO.HIGH)
+            # 역방향
+            if(setha < 0) :
+                IO.output(AIN1, IO.LOW)
+                IO.output(AIN2, IO.HIGH)
 
-            # seta가 모터 각도보다 크고 control이 플러스 값이 나오게 되면 모터가 멈춤
-            if ((setha >= motorDeg) & (control >= 0)) :
+                # seta가 모터 각도보다 크고 control이 플러스 값이 나오게 되면 모터가 멈춤
+                if ((setha >= motorDeg) & (control >= 0)) :
+                    IO.output(AIN1, IO.LOW)
+                    IO.output(AIN2, IO.LOW)
+                    p.ChangeDutyCycle(0)
+
+                # 모터가 멈추기 전까지 control과 속도를 비교하여 최소값으로 모터 속도가 정해져서 돌아감
+                p.ChangeDutyCycle(min(abs(control), 100))
+
+            # 정방향
+            elif (setha > 0) :
+                IO.output(AIN1, IO.HIGH)
+                IO.output(AIN2, IO.LOW)
+
+                # seta가 모터 각도보다 작고 control이 마이너스 값이 나오게 되면 모터가 멈춤
+                if((setha <= motorDeg) & (control <= 0)) :
+                    IO.output(AIN1, IO.LOW)
+                    IO.output(AIN2, IO.LOW)
+                    p.ChangeDutyCycle(0)
+
+                # 모터가 멈추기 전까지 control과 속도를 비교하여 최소값으로 모터 속도가 정해져서 돌아감
+                p.ChangeDutyCycle(min(abs(control), 100))
+
+            # RESET(현재 위치를 기준으로 각도를 읽어야 하므로 각도를 입력하기 전에 RESET을 해줘야 함.)
+            elif (setha == 00) :
                 IO.output(AIN1, IO.LOW)
                 IO.output(AIN2, IO.LOW)
                 p.ChangeDutyCycle(0)
 
-            # 모터가 멈추기 전까지 control과 속도를 비교하여 최소값으로 모터 속도가 정해져서 돌아감
-            p.ChangeDutyCycle(min(abs(control), 100))
+                encoderPos = 0
+                setha = 0
+                motorDeg = 0
+                error = 0
 
-        # 정방향
-        elif (setha > 0) :
-            IO.output(AIN1, IO.HIGH)
-            IO.output(AIN2, IO.LOW)
+                print('RESET')
 
-            # seta가 모터 각도보다 작고 control이 마이너스 값이 나오게 되면 모터가 멈춤
-            if((setha <= motorDeg) & (control <= 0)) :
-                IO.output(AIN1, IO.LOW)
-                IO.output(AIN2, IO.LOW)
-                p.ChangeDutyCycle(0)
+            print('setha = %d' %(setha))
+            print('enc = %d, deg = %5.1f, err = %5.1f, ctrl = %7.1f' %(encoderPos, motorDeg, error, control))
+            print('P-term = %7.1f' %(kp*error))
 
-            # 모터가 멈추기 전까지 control과 속도를 비교하여 최소값으로 모터 속도가 정해져서 돌아감
-            p.ChangeDutyCycle(min(abs(control), 100))
-
-        # RESET(현재 위치를 기준으로 각도를 읽어야 하므로 각도를 입력하기 전에 RESET을 해줘야 함.)
-        elif (setha == 00) :
-            IO.output(AIN1, IO.LOW)
-            IO.output(AIN2, IO.LOW)
-            p.ChangeDutyCycle(0)
-
-            encoderPos = 0
-            setha = 0
-            motorDeg = 0
-            error = 0
-
-            print('RESET')
-
-        print('setha = %d' %(setha))
-        print('enc = %d, deg = %5.1f, err = %5.1f, ctrl = %7.1f' %(encoderPos, motorDeg, error, control))
-        print('P-term = %7.1f' %(kp*error))
-
-        time.sleep(dt_sleep)
+            time.sleep(dt_sleep)
 
 # Crtl + c 누르면 모터 작동 멈춤
 except KeyboardInterrupt: 
