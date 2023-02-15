366a974f3ea1dcb850035ddd07d0d02de791f094
tae2On <djs99870@naver.com>
2023-02-15 17:26:00 +0900

    Update 10.py

--- 10.py
+++ 10.py
@@ -207,12 +207,12 @@ try :
     
         # 오른쪽 -------------------------------------------------
         elif (target_direction == 'right') :  
-            IO.output(AIN1, IO.HIGH)
-            IO.output(AIN2, IO.LOW)
+            IO.output(AIN1, IO.LOW)
+            IO.output(AIN2, IO.HIGH)
             IO.output(BIN3, IO.LOW)
             IO.output(BIN4, IO.HIGH)
             time.sleep(0.01)
-            p1.ChangeDutyCycle(min(abs(controlB), 0))
+            p1.ChangeDutyCycle(min(abs(controlB), 100))
             p2.ChangeDutyCycle(min(abs(controlB), 30))     #controlA 
 
             print('각도 = %5.1f' %(motorDegB))
