ce1c7baf571dc6f8f5df3367dac8d782784ed083
tae2On <djs99870@naver.com>
2023-02-17 01:48:00 +0900

    Create 11.py

---
+++ 11.py
@@ -0,0 +1,63 @@
+# 모터 속도 측정하기 (RPM, deg/s) 
+
+import RPi.GPIO as IO
+import time 
+
+pwmPin = 14
+AIN1 = 15
+AIN2 = 18
+encPinA = 2
+encPinB = 3
+
+IO.setmode(IO.BCM)
+IO.setwarnings(False)
+IO.setup(encPinA, IO.IN, pull_up_down=IO.PUD_UP)
+IO.setup(encPinB, IO.IN, pull_up_down=IO.PUD_UP)
+IO.setup(pwmPin,IO.OUT, initial=IO.LOW)
+IO.setup(AIN1,IO.OUT, initial=IO.LOW)
+IO.setup(AIN2,IO.OUT, initial=IO.LOW)
+
+p = IO.PWM(14, 100)
+p.start(0)
+
+encoderPos = 0
+
+def encoderA(encPinA):
+    global encoderPos
+    if IO.input(encPinA) == IO.input(encPinB):
+        encoderPos += 1 
+    else:
+        encoderPos -= 1
+   
+def encoderB(encPinB):
+    global encoderPos
+    if IO.input(encPinA) == IO.input(encPinB):
+        encoderPos -= 1
+    else:
+        encoderPos += 1
+
+IO.add_event_detect(encPinA, IO.BOTH, callback=encoderA)
+IO.add_event_detect(encPinB, IO.BOTH, callback=encoderB)
+
+edge = 4        # 신호의 개수 
+PPR = 9400      # PPR = 회전당 pulse수 
+
+try:
+    t = int(input("시간을 입력하시오 : "))
+   
+    while True:   
+        IO.output(AIN1, IO.HIGH)
+        # IO.output(AIN2, IO.LOW)
+        p.ChangeDutyCycle(100)
+
+        RPM = (60*edge)/(t*PPR)
+
+        print('RPM : %d' %(RPM))
+        print('PinA : %d, encoder : %d' %(encPinA, encoderPos))
+        print('PinB : %d, encoder : %d' %(encPinB, encoderPos))
+    
+        time.sleep(0.5)
+
+# Crtl + c 누르면 모터 작동 멈춤
+except KeyboardInterrupt: 
+    pass
