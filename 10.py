06cbbaa78f5a9fb4ee6fa5656d298b5849dde897
tae2On <djs99870@naver.com>
2023-02-16 15:49:27 +0900

    Update 10.py

--- 10.py
+++ 10.py
@@ -212,8 +212,8 @@ try :
             IO.output(BIN3, IO.LOW)
             IO.output(BIN4, IO.HIGH)
             time.sleep(0.01)
-            p1.ChangeDutyCycle(min(abs(controlB), 100))    #오른쪽    
-            p2.ChangeDutyCycle(min(abs(controlB), 0))     #controlA 
+            p1.ChangeDutyCycle(min(abs(controlB), 0))    #오른쪽    
+            p2.ChangeDutyCycle(min(abs(controlB), 100))     #controlA 
 
             print('각도 = %5.1f' %(motorDegB))
             print('원하는 각도 = %5.1f' %(target_deg))
