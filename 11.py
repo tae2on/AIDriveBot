9cc5d1598a9192d772f805d24e5a10cf526a54f0
tae2On <djs99870@naver.com>
2023-02-17 03:00:17 +0900

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,30 +1,35 @@
 # 모터 속도 측정하기 (RPM, deg/s) 
 
 import RPi.GPIO as IO
-import time 
+import time
 
-pwmPin = 14
-AIN1 = 15
-AIN2 = 18
-encPinA = 2
-encPinB = 3
+IO.setmode(IO.BOARD)
 
-IO.setmode(IO.BCM)
-IO.setwarnings(False)
-IO.setup(encPinA, IO.IN, pull_up_down=IO.PUD_UP)
-IO.setup(encPinB, IO.IN, pull_up_down=IO.PUD_UP)
-IO.setup(pwmPin,IO.OUT, initial=IO.LOW)
-IO.setup(AIN1,IO.OUT, initial=IO.LOW)
-IO.setup(AIN2,IO.OUT, initial=IO.LOW)
+AIN1= 13
+AIN2= 15   
+PWMA= 12
 
+# 듀티 비를 변화시킬 스텝 정의 
+c_step = 10   
 
-p = IO.PWM(14, 100)
-p.start(100)
+#각 핀을 출력 핀으로 설정 
+IO.setup(AIN1, IO.OUT, initial=IO.LOW)
+IO.setup(AIN2, IO.OUT, initial=IO.LOW)
+IO.setup(PWMA, IO.OUT, initial=IO.LOW)
 
-IO.output(AIN1, IO.HIGH)
-IO.output(AIN2, IO.LOW)
-p.ChangeDutyCycle(100)
+#PWM 객체 인스턴스 작성(출력 핀:12, 주파수 100)
+p = IO.PWM(PWMA, 100)
+p.start(0)
 
-print('회전')
+try:
+	while 1:
+		IO.output(AIN1, IO.HIGH)
+        # IO.output(AIN2, IO.LOW)
+        p.ChangeDutyCycle(100)
 
+except KeyboardInterrupt: 
+	pass 
 
+p.stop() 
+
+IO.cleanup()
