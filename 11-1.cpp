103ddd17a2472e8a1d4b17299610a7ab3f94112d
tae2On <djs99870@naver.com>
2023-03-17 00:35:51 +0900

    Update 11-1.cpp

--- 11-1.cpp
+++ 11-1.cpp
@@ -81,7 +81,7 @@ void goFront() {
     cout << "ctrlA = " << controlA << ", degA = " << motorDegA << ", errA = " << errorA << ", disA = " << motor_distanceA << ", derrA = " << derrorA << endl;
     cout << "ctrlB = " << controlB << ", degB = " << motorDegB << ", errB = " << errorB << ", disB = " << motor_distanceB << ", derrB = " << derrorB << endl;
 
-    return call(lidar_way);
+    return call();
 }
 
 /* 후진 */
@@ -98,7 +98,7 @@ void goBack() {
     cout << "ctrlA = " << controlA << ", degA = " << motorDegA << ", errA = " << errorA << ", disA = " << motor_distanceA << ", derrA = " << derrorA << endl;
     cout << "ctrlB = " << controlB << ", degB = " << motorDegB << ", errB = " << errorB << ", disB = " << motor_distanceB << ", derrB = " << derrorB << endl;
    
-    return call(lidar_way);
+    return call();
 }
 
 /* 정지 */
@@ -115,7 +115,7 @@ void Stop() {
     cout << "ctrlA = " << controlA << ", degA = " << motorDegA << ", errA = " << errorA << ", disA = " << motor_distanceA << ", derrA = " << derrorA << endl;
     cout << "ctrlB = " << controlB << ", degB = " << motorDegB << ", errB = " << errorB << ", disB = " << motor_distanceB << ", derrB = " << derrorB << endl;
 
-    return call(string(lidar_way));
+    return call();
 }
 
 /* 방향 설정하기 */
