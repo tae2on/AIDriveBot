8dce6e9d384b741421bb1c98bd04edf442159f40
tae2On <djs99870@naver.com>
2023-03-21 16:29:44 +0900

    Update 11-1.cpp

--- 11-1.cpp
+++ 11-1.cpp
@@ -98,7 +98,7 @@ void Stop();
 
 /* 방향 설정하기 */
 // 변수명 수정하기 
-void call(lidar_way) {
+void call(string lidar_way) {
     // 전진
     if (lidar_way == "goFront") {
         goFront();
@@ -237,15 +237,13 @@ int main(){
         string lidar_way;
         std::cout << "값을 입력하시오 : ";
         std::cin >> lidar_way;
-        return call(lidar_way);
         
-
         void call();
         void goFront();
         void goBack();
         void Stop();
 
-        call();
+        call(lidar_way);
         delay(1000);
     }
 }
