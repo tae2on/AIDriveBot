ce6323600ee8c8f6e0737a8a14048346f3c7d953
tae2On <djs99870@naver.com>
2023-03-25 11:01:58 +0900

    Update 11-1.cpp

--- 11-1.cpp
+++ 11-1.cpp
@@ -159,7 +159,7 @@ void Calculation() {
         turn_deg = carDistance / wheelbase;
 
         // 보정 계수
-        carMoving_filter = 0.6;     //0.4<x<0.5  
+        carMoving_filter = 0.4;     //0.4<x<0.5  
         turn_deg *= carMoving_filter;
 
         cout << "각도 = " << motorDegA << endl;
