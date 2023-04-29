1d1a069ad15843784487ac1ba1615f9663cc6465
tae2On <djs99870@naver.com>
2023-04-30 03:15:51 +0900

    Update 7-1.py

--- 7-1.py
+++ 7-1.py
@@ -163,6 +163,8 @@ try :
             print('원하는 각도 = %5.1f' %(target_deg))
             print('ctrlA = %7.1f, degA = %5.1f, errA = %5.1f, disA = %5.1f, derrA = %5.1f' %(controlA, motorDegA, errorA, motor_distanceA, derrorA))  
             print('ctrlB = %7.1f, degB = %5.1f,s errB = %5.1f, disB = %5.1f, derrB = %5.1f' %(controlB, motorDegB, errorB, motor_distanceB, derrorB))  
+            print('encA = %5.1f' %(encoderPosA))
+            print('encB = %5.1f' %(encoderPosB))
 
             if ((motorDegA >= target_deg)):
                 IO.output(AIN1, IO.LOW)
