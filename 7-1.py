f143c5684e3e8e392f33ca1326010ee2b7180709
tae2On <djs99870@naver.com>
2023-04-30 04:23:42 +0900

    Update 7-1.py

--- 7-1.py
+++ 7-1.py
@@ -153,10 +153,10 @@ try :
 
         # 전진 -------------------------------------------------
         if (target_direction == 'front') :  
-            IO.output(AIN1, IO.LOW)
-            IO.output(AIN2, IO.HIGH)
-            IO.output(BIN3, IO.HIGH)
-            IO.output(BIN4, IO.LOW)
+            IO.output(AIN1, IO.HIGH)
+            IO.output(AIN2, IO.LOW)
+            IO.output(BIN3, IO.LOW)
+            IO.output(BIN4, IO.HIGH)
             time.sleep(0.01)
             p1.ChangeDutyCycle(min(abs(controlA), 100))
             p2.ChangeDutyCycle(min(abs(controlB), 100))
