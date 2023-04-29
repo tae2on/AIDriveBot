400a1f0aa65efa09fe1c02925be939ee347ad551
tae2On <djs99870@naver.com>
2023-04-29 23:29:38 +0900

    Update 7-1.py

--- 7-1.py
+++ 7-1.py
@@ -43,8 +43,7 @@ IO.add_event_detect(encPinB, IO.BOTH, callback=encoderB)
 targetDeg = 360.
 
 # PID 제어
-ratio = 360./144./4000.
-
+ratio = 1. /144.
 # PID 상수
 kp = float(input("KP:"))   #0.35
 kd = float(input("KD:"))   #0.
