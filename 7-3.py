9477d0965bd997c01da0beb7041bf006a42e5285
tae2On <djs99870@naver.com>
2023-05-02 18:52:59 +0900

    Update 7-3.py

--- 7-3.py
+++ 7-3.py
@@ -10,11 +10,11 @@ encPinA = 2 # 보라색 (A)
 encPinB = 3 # 파랑색 (B)
 
 
-IO.setmode(IO.BCM)
-IO.setwarnings(False)
-IO.setup(pwmPinA, IO.OUT, initial=IO.LOW)
-IO.setup(AIN1, IO.OUT, initial=IO.LOW)
-IO.setup(AIN2, IO.OUT, initial=IO.LOW)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(pwmPinA, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(AIN1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(AIN2, GPIO.OUT, initial=GPIO.LOW)
 
 GPIO.setup(encPinA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(encPinB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -39,8 +39,8 @@ GPIO.add_event_detect(encPinA, GPIO.BOTH, callback=encoderA)
 GPIO.add_event_detect(encPinB, GPIO.BOTH, callback=encoderB)
 
 while True:
-    IO.output(AIN1, 1) # IN1 핀을 HIGH(1)으로 설정
-    IO.output(AIN2, 0) # IN2 핀을 LOW(0)으로 설정
+    GPIO.output(AIN1, 1) # IN1 핀을 HIGH(1)으로 설정
+    GPIO.output(AIN2, 0) # IN2 핀을 LOW(0)으로 설정
     p1.ChangeDutyCycle(50) # PWM 신호의 duty cycle을 50%로 설정
     print(encoderPosA)
     time.sleep(0.1)
