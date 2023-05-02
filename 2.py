9366495581ff328e1adadac069b7e27679d26a2b
tae2On <djs99870@naver.com>
2023-05-02 19:07:25 +0900

    Update 2.py

--- 2.py
+++ 2.py
@@ -42,7 +42,7 @@ try :
     while True:
         IO.output(AIN1, IO.HIGH)
         # IO.output(AIN2, IO.LOW)
-        p.ChangeDutyCycle(100)
+        p.ChangeDutyCycle(50)
 
         print('PinA : %d, encoder : %d' %(encPinA, encoderPos))
         print('PinB : %d, encoder : %d' %(encPinB, encoderPos))
