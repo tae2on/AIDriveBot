2bf40b3210083e8e2b4244df3d0ca0fe850596c2
tae2On <djs99870@naver.com>
2023-05-03 21:25:48 +0900

    Update 7-3.py

--- 7-3.py
+++ 7-3.py
@@ -43,7 +43,7 @@ IO.add_event_detect(encPinB, IO.BOTH, callback=encoderB)
 targetDeg = 360.
 
 # PID 제어
-ratio = 360./144./9.027. # 한 바퀴에 약 1350펄스 (정확하지 않음 - 계산값)
+ratio = 360./144./9.027 # 한 바퀴에 약 1350펄스 (정확하지 않음 - 계산값)
 
 # PID 상수
 kp = float(input("kp: "))
