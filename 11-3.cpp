a50b5e1a187b087947ea672ec18c995a9158fbb4
tae2On <djs99870@naver.com>
2023-05-06 21:20:00 +0900

    Update 11-3.cpp

--- 11-3.cpp
+++ 11-3.cpp
@@ -200,15 +200,9 @@ void MotorControl::call(int x){
         cout << "왼쪽 모터 오차값 = " << derrorA << endl;
         cout << "오른쪽 모터 이동거리  = " << motor_distance_B << endl;
         cout << "오른쪽 모터 오차값 = " << derrorB << endl;
-
+        cout << "모터 각도 "<< motorDegB << endl;
         // x(방향)의 값이 1(전진)이 아닐 경우 x(방향)을 다시 입력 받음 
         if(x != 1){
-            // 이동거리 출력 
-            cout << "왼쪽 모터 이동거리A  = " << motor_distance_A << endl;
-            cout << "왼쪽 모터 오차값 = " << derrorA << endl;
-            cout << "오른쪽 모터 이동거리  = " << motor_distance_B << endl;
-            cout << "오른쪽 모터 오차값 = " << derrorB << endl;
-
             x = getInput();
         }
     }
