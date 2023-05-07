223cadd91ccdbaadf88bec7a532c5dd1d3965143
tae2On <djs99870@naver.com>
2023-05-07 16:20:42 +0900

    Update 9-1.py

--- 9-1.py
+++ 9-1.py
@@ -111,7 +111,7 @@ r = 11.5    # 타이어의 반지름
 # 모터 이동 거리 (r: 11.5) 
 try :
     target_direction = 'front' #str(input("이동하고 싶은 방향을 입력하시오 : ")) 
-    target_distance = 30 #int(input("이동하고 싶은 거리를 입력하시오: "))
+    target_distance = int(input("이동하고 싶은 거리를 입력하시오: "))
     
     while True: 
         wheel = 2*math.pi*r     #원둘레 = 72.26
