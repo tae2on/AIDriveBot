0d8a136568c9e2c02bfd0992a9ee5d251e3d5c92
tae2On <djs99870@naver.com>
2023-05-07 21:54:34 +0900

    Update 12.cpp

--- 12.cpp
+++ 12.cpp
@@ -136,7 +136,7 @@ int main(){
 
     encoderPosLeft = 0;
     encoderPosRight = 0;
-    
+
     cout << "kp의 값 : ";
     cin >> kp;
     cout << "ki의 값 : ";
@@ -195,8 +195,8 @@ int main(){
         delay(10);
         // 속도 설정 
 
-        PwmWrite(pwmPinA, min(abs(controlA), 255.0));  
-        PwmWrite(pwmPinB, min(abs(controlB), 255.0));  
+        pwmWrite(pwmPinA, min(abs(controlA), 255.0));  
+        pwmWrite(pwmPinB, min(abs(controlB), 255.0));  
         // analogWrite(pwmPinA, min(abs(controlA), 0.0));
         //analogWrite(pwmPinB, min(abs(controlB), 255.0));
 
