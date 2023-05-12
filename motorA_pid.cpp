0dde1b670bf8d24fb16e49132068987e050c56e7
tae2On <djs99870@naver.com>
2023-05-12 17:54:52 +0900

    Update motorA_pid.cpp

--- motorA_pid.cpp
+++ motorA_pid.cpp
@@ -136,7 +136,7 @@ int main(){
         cout << "encA = " << encoderPosLeft << endl;
         cout << "회전 각도 = " << motorDegA << endl;
             
-        if (motorDegA >= target_deg){
+        if (encoderPosLeft >= 400){
             digitalWrite(AIN1, LOW);
             digitalWrite(AIN2, LOW);       
             delay(10);
