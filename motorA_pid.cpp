6b4895dd15f00ad81bda699cc534837cc011fb92
tae2On <djs99870@naver.com>
2023-05-13 11:55:02 +0900

    Update motorA_pid.cpp

--- motorA_pid.cpp
+++ motorA_pid.cpp
@@ -132,14 +132,13 @@ int main(){
         cout << "회전 각도 = " << motorDegA << endl;
             
         if (motorDegA >= target_deg){
-            softPwmWrite(pwmPinA, 0); 
             digitalWrite(AIN1, LOW);
             digitalWrite(AIN2, LOW);       
             delay(10);
             // 속도 설정 
-            
             controlA = 0;
-            encoderPosLeft = 0;
+            softPwmWrite(pwmPinA, 0); 
+            
         }
     }    
     return 0; 
