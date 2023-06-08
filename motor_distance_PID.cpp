94a15cd134df6511e01166e8e23ac256d4395f1e
tae2On <djs99870@naver.com>
2023-06-08 20:40:28 +0900

    Update motor_distance_PID.cpp

--- motor_distance_PID.cpp
+++ motor_distance_PID.cpp
@@ -206,10 +206,10 @@ int main(){
         error_prev_s = error_s;
 
         // 방향 설정 
-        digitalWrite(AIN1, LOW);
-        digitalWrite(AIN2, HIGH);
-        digitalWrite(BIN3, HIGH);
-        digitalWrite(BIN4, LOW);
+        digitalWrite(AIN1, HIGH);
+        digitalWrite(AIN2, LOW);
+        digitalWrite(BIN3, LOW);
+        digitalWrite(BIN4, HIGH);
         // 속도 설정 
         softPwmWrite(pwmPinA, min(abs(control_L), 48.));     
         softPwmWrite(pwmPinB, min(abs(control_R), 50.)); 
