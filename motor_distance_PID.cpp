655fce333fc4d3a8247213d647b2892125a0da1a
tae2On <djs99870@naver.com>
2023-06-09 15:59:50 +0900

    Update motor_distance_PID.cpp

--- motor_distance_PID.cpp
+++ motor_distance_PID.cpp
@@ -193,8 +193,7 @@ int main(){
         cout << "ctrlL = " << control_L << ", ctrlR = " << control_R << endl;
         cout << "y = " << y_coordinate << ", y_prev = " << y_prev_coordinate << endl;
         cout << "error_d = " << error_d << ", error_prev_d = " << error_prev_d << ", error_prev_prev_d = " << error_prev_prev_d << endl;        
-        cout << "지난 시간: " << duration.count() << "밀리초" << endl;
- 
+        
         // 왼쪽 DC모터 
         delta_vL =  - kp_dL * (error_d - error_prev_d) - ki_dL * error_d - kd_dL * (error_d - 2 * error_prev_d + error_prev_prev_d) + kp_sL * (error_s - error_prev_s) + ki_sL * error_s + kd_sL * (error_s - 2 * error_prev_s + error_prev_prev_s);
         control_L += delta_vL;
@@ -238,6 +237,9 @@ int main(){
       std::chrono::milliseconds duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);// 루프 실행 시간 계산
       std::this_thread::sleep_for(std::chrono::milliseconds(10) - duration);  // 루프 실행 시간이 10ms가 되도록 대기
  
+      cout << "지난 시간: " << duration.count() << "밀리초" << endl;
+ 
+
     }    
   return 0; 
 }  
