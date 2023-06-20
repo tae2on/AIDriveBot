43ce5293f013df474b6bc6a220371cadbe21642e
tae2On <djs99870@naver.com>
2023-06-21 03:42:40 +0900

    Update motor.cpp

--- motor.cpp
+++ motor.cpp
@@ -55,11 +55,11 @@ float kp_dR = 6; // 거리 : 5
 float kd_dR = 0.4; // 거리 : 0.4
 float ki_dR = 0; // 0 
 
-float kp_sL = 8; 
+float kp_sL = 0; 
 float kd_sL = 0;        
 float ki_sL = 0; 
  
-float kp_sR = 6;  
+float kp_sR = 0;  
 float kd_sR = 0;        
 float ki_sR = 0; 
 
