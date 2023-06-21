be6b1c1a7616d356ca5c8a0f1e4b6117e8b233a1
tae2On <djs99870@naver.com>
2023-06-21 19:44:49 +0900

    Update motor1.cpp

--- motor1.cpp
+++ motor1.cpp
@@ -453,8 +453,8 @@ void MotorControl::call(InputData input){
                   // 방향 설정 
                   digitalWrite(AIN1, LOW);
                   digitalWrite(AIN2, HIGH);
-                  digitalWrite(BIN3, HIGH);
-                  digitalWrite(BIN4, LOW);
+                  digitalWrite(BIN3, LOW);
+                  digitalWrite(BIN4, HIGH);
             
                   // 속도 설정 
                   softPwmWrite(pwmPinA, min(pwmL, 52));     
