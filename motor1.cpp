0a136772bdf74a1153208c26f2bf4e75dab3f345
tae2On <djs99870@naver.com>
2023-06-21 19:56:52 +0900

    Update motor1.cpp

--- motor1.cpp
+++ motor1.cpp
@@ -430,8 +430,8 @@ void MotorControl::call(InputData input){
             // 방향 조절 
             digitalWrite(AIN1, LOW);
             digitalWrite(AIN2, HIGH);
-            digitalWrite(BIN3, LOW);
-            digitalWrite(BIN4, HIGH);
+            digitalWrite(BIN3, HIGH);
+            digitalWrite(BIN4, LOW);
             
             // 속도 설정 
             softPwmWrite(pwmPinA, min(pwmL, 70));     
@@ -459,29 +459,30 @@ void MotorControl::call(InputData input){
                   // 속도 설정 
                   softPwmWrite(pwmPinA, min(pwmL, 52));     
                   softPwmWrite(pwmPinB, min(pwmR, 59));                
-            
-                    if (abs(error_d) <= tolerance) {
-                      // 방향 설정 
-                      digitalWrite(AIN1, LOW);
-                      digitalWrite(AIN2, LOW);
-                      digitalWrite(BIN3, LOW);
-                      digitalWrite(BIN4, LOW);
-                      // 속도 설정 
-                      softPwmWrite(pwmPinA, 0);
-                      softPwmWrite(pwmPinB, 0);
+                }
+                    
+                else if (abs(error_d) <= tolerance) {
+                  // 방향 설정 
+                  digitalWrite(AIN1, LOW);
+                  digitalWrite(AIN2, LOW);
+                  digitalWrite(BIN3, LOW);
+                  digitalWrite(BIN4, LOW);
+                  // 속도 설정 
+                  softPwmWrite(pwmPinA, 0);
+                  softPwmWrite(pwmPinB, 0);
 
-                      prev_distance_robot = distance_robot;
+                  prev_distance_robot = distance_robot;
 
-                      auto end = std::chrono::high_resolution_clock::now();  // 루프 종료 시간 기록
-                      auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-                      std::cout << "지난 시간: " << duration.count() << "밀리초" << std::endl;
-                      del_ts = duration.count();
+                  auto end = std::chrono::high_resolution_clock::now();  // 루프 종료 시간 기록
+                  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+                  std::cout << "지난 시간: " << duration.count() << "밀리초" << std::endl;
+                  del_ts = duration.count();
 
-                      input = getInput();
-                    }
-                }       
-            }
-          }
+                  input = getInput();
+                }
+            }       
+        }
+
         // 2사분면 
           if ((input.x_target_coordinate < 0) && (input.y_target_coordinate > 0)){
             // 방향 조절 
